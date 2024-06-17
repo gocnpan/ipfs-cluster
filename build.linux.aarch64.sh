b252dc26ca53fa2e49a5ec9dcbe8fe34b11d8a70
pan_ding_rong <panseng.dr@qq.com>
2024-06-17 10:43:49 +0800

    build: 增加构建linux arm64脚本

---
+++ build.linux.aarch64.sh
@@ -0,0 +1,10 @@
+g use 1.22.3
+
+export CGO_ENABLED=0
+export GOOS=linux
+export GOARCH=arm64
+
+# 通过 在根目录 make build
+# 获取 编译的信息
+cd `pwd`/cmd/ipfs-cluster-service
+go build "-trimpath" -mod=readonly -ldflags "-X main.commit=v110ap" -o "cluster.aarch64" .
